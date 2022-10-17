25f7e170ff2f26f897ad3dd8ef7064075a866046
Marcos Miguel dos Santos <114240224+setfocus2022@users.noreply.github.com>
2022-10-17 16:50:28 -0300

    pizza

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -32,10 +32,10 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Home | Eletro Santa Clara</title>
+        <title>Home | La Casa Pizza Bar</title>
         <meta
           name="description"
-          content="Eletro Santa Clara - Referência em Qualidade e Ótimos Preços em Lençóis Pta e Região"
+          content="La Casa Pizza Bar"
         />
         <meta
           name="keywords"
